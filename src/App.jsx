6508cfa1796462fd2d30c47a7bf44a4b1d5aca10
Nihat <abdullazadenihat@gmail.com>
2022-12-28 19:36:16 +0400

    About page added

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,15 @@ import { Route, Routes } from 'react-router-dom'
 import './assets/css/style.scss'
 
 import HomePage from './pages/Home'
+import AboutPage from './pages/About'
+
 function App() {
 
   return (
     <div className="app">
       <Routes>
         <Route path='' element={<HomePage />} />
+        <Route path='/about' element={<AboutPage />} />
       </Routes>
     </div>
   )
--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,28 +1,38 @@
-import React from 'react'
+
+import './About.scss'
+
+import Nav from '../components/Nav'
+import Footer from '../components/Footer'
+
+import foundingImage1 from '../assets/img/founding-1.jpg'
+import growthImage1 from '../assets/img/growth-1.jpg'
+
 
 export default function About() {
   return (
     <div className='about-page'>
+      <Nav />
       <section className="hero">
-        <div className="container-1300">
-          <h2 className="heading">Our mission is to make building websites easier for everyone. </h2>
-          <p className="text">
-            Start Bootstrap was built on the idea that quality, functional website templates and themes should be available to everyone. Use our open source, free products, or support us by purchasing one of our premium products or services.
-          </p>
-          <div className="action">
-            <button> Read our story </button>
+        <div className="container">
+          <div className="row">
+            <h2 className="heading">Our mission is to make building websites easier for everyone. </h2>
+            <p className="subheading">
+              Start Bootstrap was built on the idea that quality, functional website templates and themes should be available to everyone. Use our open source, free products, or support us by purchasing one of our premium products or services.
+            </p>
+            <div className="action">
+              <button> Read our story </button>
+            </div>
           </div>
         </div>
       </section>
-
       <section className="founding">
-        <div className="container-1300">
+        <div className="container">
           <div className="row">
             <figure className="column left-column ">
-              <img src="" alt="" />
+              <img src={foundingImage1} alt="" />
             </figure>
             <div className="column right-column ">
-              <h3 className="heading">Our founding </h3>
+              <h3 className="title">Our founding </h3>
               <p className="text"> Lorem ipsum dolor sit amet consectetur adipisicing elit. Iusto est, ut esse a labore aliquam beatae expedita. Blanditiis impedit numquam libero molestiae et fugit cupiditate, quibusdam expedita, maiores eaque quisquam. </p>
             </div>
           </div>
@@ -30,48 +40,48 @@ export default function About() {
       </section>
 
       <section className="growth">
-        <div className="container-1300">
+        <div className="container">
           <div className="row">
-            <div className="column left-column ">
-              <h3 className="heading">Our founding </h3>
-              <p className="text"> Lorem ipsum dolor sit amet consectetur adipisicing elit. Iusto est, ut esse a labore aliquam beatae expedita. Blanditiis impedit numquam libero molestiae et fugit cupiditate, quibusdam expedita, maiores eaque quisquam. </p>
+            <div className="left-column">
+              <h3 className="title"> Growth & Beyond </h3>
+              <p className="text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Iusto est, ut esse a labore aliquam beatae expedita. Blanditiis impedit numquam libero molestiae et fugit cupiditate, quibusdam expedita, maiores eaque quisquam.</p>
             </div>
-            <figure className="column left-column ">
-              <img src="" alt="" />
+            <figure className="right-column">
+              <img src={growthImage1} alt="" />
             </figure>
           </div>
         </div>
       </section>
 
       <section className="team">
-        <div className="container-1300">
-          <h3 className="heading"> Our team </h3>
-          <p className="text">Dedicated to quality and your success </p>
+        <div className="container">
+          <h3 className="heading">Our team</h3>
+          <p className="subheading"> Dedicated to quality and your success</p>
           <div className="row">
-            <TeamBox name="Ibbie Eckart" position="Founder & CEO" />
-            <TeamBox name="Arden Vasak " position="CFO" />
-            <TeamBox name="Toribio Nerthus " position="Operations Manager" />
-            <TeamBox name="Malvina Calla" position="CTO" />
+            <TeamBox img={foundingImage1} fullname="Nihat A" position="President of company" />
+            <TeamBox img={foundingImage1} fullname="Nihat A" position="President of company" />
+            <TeamBox img={foundingImage1} fullname="Nihat A" position="President of company" />
+            <TeamBox img={foundingImage1} fullname="Nihat A" position="President of company" />
           </div>
         </div>
       </section>
+
+
 
       <Footer />
 
     </div>
   )
 }
-
-
-function TeamBox() {
+function TeamBox(props) {
   return (
-    <div className="box">
+    <div className="box" >
       <figure>
-        <img src="" alt="" />
+        <img src={props.img} alt="" />
       </figure>
-      <h4 className="heading">Nihat Abdullazade </h4>
-      <p className="text"> Founder & CEO </p>
-    </div>
+      <h4 className="fullname"> {props.fullname} </h4>
+      <p className="position"> {props.position} </p>
+    </div >
   )
 
 }
